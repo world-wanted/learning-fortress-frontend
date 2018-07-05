6cb4afed81f1e3ff6cf18e12a95fb9e68396d924
RealFighter64 <realfighter64@gmail.com>
2018-07-05 21:10:39 +0100

    fix server deployment

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+app.use(express.static(__dirname + '/dist/learning-fortress-frontend'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
