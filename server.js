915ad01146fec7350e943aaa38a8bb6325e4dacf
RealFighter64 <realfighter64@gmail.com>
2018-07-12 10:16:58 +0100

    Fix application serving Fix /fortress BUG

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/learning-fortress-frontend'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/learning-fortress-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
