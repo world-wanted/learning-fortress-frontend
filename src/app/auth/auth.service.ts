942f5fd9c9da848462453437643f00da1781bd20
RealFighter64 <realfighter64@gmail.com>
2018-07-26 15:21:12 +0100

    Improve home screen including personalization.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "angularfire2/auth";
+import { User } from "firebase";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    constructor(public afAuth: AngularFireAuth) { }
+    user: Observable<User>;
+
+    constructor(public afAuth: AngularFireAuth) {
+        this.user = afAuth.user;
+        this.user.subscribe((user) => {
+            console.log(user.email);
+        })
+    }
 
     logout() {
         this.afAuth.auth.signOut();
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,15 +11,16 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
     imports: [
         MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule,
         MatListModule, MatChipsModule, MatProgressSpinnerModule, MatExpansionModule,
-        MatStepperModule, MatButtonToggleModule, MatCardModule],
+        MatStepperModule, MatButtonToggleModule, MatCardModule, MatMenuModule],
     exports: [
         MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule,
         MatListModule, MatChipsModule, MatProgressSpinnerModule, MatExpansionModule,
-        MatStepperModule, MatButtonToggleModule, MatCardModule],
+        MatStepperModule, MatButtonToggleModule, MatCardModule, MatMenuModule],
 })
 export class MaterialModule {  }
