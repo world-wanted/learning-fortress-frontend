5c8c32ea8d78c066ce9c37da992458c9b4529c14
unknown <william.gooch.wg@gmail.com>
2018-08-16 15:51:14 +0100

    Switch pallet color scheme depending on the subject of the pallet

--- src/app/fortress/pallet.component.ts
+++ src/app/fortress/pallet.component.ts
@@ -11,13 +11,15 @@ import { Observable } from "rxjs";
 })
 export class PalletComponent {
     palletId: string;
+    pallet: Observable<Pallet>;
     bricks: Observable<Brick[]>;
 
     constructor(public database: DatabaseService, public route: ActivatedRoute) {
         this.route.paramMap
             .subscribe((data: ParamMap) => {
                 this.palletId = data.get('id');
-                database.getPallet(this.palletId).subscribe((pallet) => {
+                this.pallet = database.getPallet(this.palletId)
+                this.pallet.subscribe((pallet) => {
                     this.bricks = database.getBricks(pallet);
                 })
             })
