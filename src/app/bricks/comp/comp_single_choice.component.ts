b9409c09656672545a4fadd388a8ca0a3a6b8c55
William Gooch <william.gooch.wg@gmail.com>
2018-08-22 14:26:05 +0100

    fix #29 and #27

--- src/app/bricks/comp/comp_single_choice.component.ts
+++ src/app/bricks/comp/comp_single_choice.component.ts
@@ -25,8 +25,8 @@ export class CompSingleChoice extends Comp {
                 <mat-checkbox *ngIf="attempt"  [checked]="getState(choice) == 1" [indeterminate]="getState(choice) == -1" disabled></mat-checkbox>
                 <div fxFlex="1 0 0"></div>
                 <div fxLayout="column">
-                    <div>{{ choice }}</div>
-                    <div *ngIf="attempt" style="font-size: 20px">{{ data.data.reveals[getChoice(choice)] }}</div>
+                    <div style="line-height: 4vw;">{{ choice }}</div>
+                    <div *ngIf="attempt" style="line-height: 2.5vw; font-size: 2.5vw">{{ data.data.reveals[getChoice(choice)] }}</div>
                 </div>
                 <div fxFlex="1 0 0"></div>
             </div>
