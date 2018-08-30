05777e1ec0b162b2c77b87a22de74514a6f201be
lindsay macvean <lindsaymacvean@gmail.com>
2018-08-30 23:32:27 +0100

    fix #54 fixed issue with minutes returning as 00 instead of 60

--- src/app/bricks/brickTime.pipe.ts
+++ src/app/bricks/brickTime.pipe.ts
@@ -2,36 +2,36 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'brickTime' })
 export class BrickTimePipe implements PipeTransform {
-    transform(value: number, type: string): Date {
+    transform(value: number, type: string): number {
         switch(type) {
             case "intro": switch(value) {
-                case 1: return new Date(300000);
-                case 2: return new Date(600000);
-                case 3: return new Date(900000);
-                default: return new Date(0);
+                case 1:  return 300000;
+                case 2:  return 600000;
+                case 3:  return 900000;
+                default: return 0;
             }
             case "live": switch(value) {
-                case 1: return new Date(500000);
-                case 2: return new Date(1000000);
-                case 3: return new Date(1500000);
-                default: return new Date(0);
+                case 1:  return 500000;
+                case 2:  return 1000000;
+                case 3:  return 1500000;
+                default: return 0;
             }
             case "summary": switch(value) {
-                case 1: return new Date(200000);
-                case 2: return new Date(400000);
-                case 3: return new Date(600000);
-                default: return new Date(0);
+                case 1:  return 200000;
+                case 2:  return 400000;
+                case 3:  return 600000;
+                default: return 0;
             }
             case "review": switch(value) {
-                case 1: return new Date(200000);
-                case 2: return new Date(400000);
-                case 3: return new Date(600000);
-                default: return new Date(0);
+                case 1:  return 200000;
+                case 2:  return 400000;
+                case 3:  return 600000;
+                default: return 0;
             } default: switch(value) {
-                case 1: return new Date(1200000);
-                case 2: return new Date(2400000);
-                case 3: return new Date(3600000);
-                default: return new Date(0);
+                case 1:  return 1200000;
+                case 2:  return 2400000;
+                case 3:  return 3600000;
+                default: return 0;
             }
         }
     }
