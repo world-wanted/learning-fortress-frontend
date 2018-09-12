906b48c0a4e06d6205ec380a2054e34bc6fffcc1
lindsaymacvean <lindsaymacvean@gmail.com>
2018-09-12 15:39:25 +0100

    updated environment prod and preprod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDZAppRL0CfjhcMo4-9l0DIIRdDXXexvx4",
+        authDomain: "learning-fortress.firebaseapp.com",
+        databaseURL: "https://learning-fortress.firebaseio.com",
+        projectId: "learning-fortress",
+        storageBucket: "learning-fortress.appspot.com",
+        messagingSenderId: "45503831861"
+    }
+};
