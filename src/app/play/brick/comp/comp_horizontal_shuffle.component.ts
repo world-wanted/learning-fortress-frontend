c225f856cc708924f039130db0fd92f453613a03
ismail mellouli <ismailmell93@gmail.com>
2018-09-27 14:58:22 +0100

    fix

--- src/app/play/brick/comp/comp_horizontal_shuffle.component.ts
+++ src/app/play/brick/comp/comp_horizontal_shuffle.component.ts
@@ -27,7 +27,7 @@ export class CompHorizontalShuffle extends Comp {
     selector: 'horizontal-shuffle',
     template: `
     <div class="horizontal-shuffle-container" [dragula]="'DRAG'" [(dragulaModel)]="userChoices" fxLayout.gt-xs="row wrap" fxLayout.xs="row wrap" fxLayoutAlign.xs="center center">
-        <mat-card fxFlex.gt-xs="0 0 0%" fxFlex.xs="0 0 0" class="horizontal-shuffle-item touch-list-item" *ngFor="let choice of userChoices; let i = index" style="padding: 0px !important;">
+        <mat-card fxFlex.gt-xs="0 0 0%" fxFlex.xs="0 0 0" class="horizontal-shuffle-item touch-list-item" *ngFor="let choice of userChoices; let i = index" style="padding: 5px !important;white-space: nowrap;">
             <div fittext>{{choice}}</div>
         </mat-card>
     </div>
