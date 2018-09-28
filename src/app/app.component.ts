2df148d7fa965d4e4a66718eb0d49e53c76b7270
Andrii Romakh <andriyromakh@gmail.com>
2018-09-28 09:43:51 +0300

    #93 Fix for IOS11 drag issue with dragula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { User } from 'firebase';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth/auth.service';
+import { DragulaService } from 'ng2-dragula';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    constructor(public authService: AuthService) { }
+    constructor(private dragula: DragulaService, public authService: AuthService) { 
+        this.dragulaIOS11Fix();
+    }
+
+    dragulaIOS11Fix() {
+        let isScrolable: Boolean = true;
+
+        const listener = function(e) {
+            if (isScrolable) {
+                e.preventDefault();
+            }
+        };
+
+        document.addEventListener('touchmove', listener, { passive: false });
+
+        this.dragula
+            .drag()
+            .subscribe(value => {
+                isScrolable = true;
+        });
+
+        this.dragula
+            .drop()
+            .subscribe(value => {
+                isScrolable = false;
+        });
+    }
 }
