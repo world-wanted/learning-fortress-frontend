df5a8fee36612940e473515b23f170566d6804ea
vb <future21star@yahoo.com>
2018-09-28 04:06:19 -0400

    fix #106

--- src/app/play/brick/comp/comp_text_highlighting.component.ts
+++ src/app/play/brick/comp/comp_text_highlighting.component.ts
@@ -18,10 +18,12 @@ export class CompTextHighlighting extends Comp {
 @Component({
     selector: 'text-highlighting',
     template: `
-    <p *ngIf="attempt" fittext>{{ data.data.reveal }}</p>
-    <p class="text-highlighting" fittext>
-        <span *ngFor="let word of words; let i = index" [highlight]="word.highlight" [state]="getState(i)" (click)="toggleHighlight(i)"> {{ word.word }} </span>
-    </p>
+    <div class="text-highlighting-wrapper">
+        <p *ngIf="attempt" fittext>{{ data.data.reveal }}</p>
+        <p class="text-highlighting" fittext>
+            <span *ngFor="let word of words; let i = index" [highlight]="word.highlight" [state]="getState(i)" (click)="toggleHighlight(i)"> {{ word.word }} </span>
+        </p>
+    </div>
     `,
     styleUrls: ['../live.component.scss']
 })
